4085de4008cf3657d8f1f7301382cd00b5588c88
Iqra Ashraf <iqraashraf3217@gmail.com>
2026-05-09 11:59:21 +0500

    Update CORS for new domain

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://campus-recruit-pk.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
